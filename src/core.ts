5d7831f13e5699a312e287afb42fc9311d48089e
cj-l <barefootdev.eth@gmail.com>
2022-01-11 13:54:01 +0800

    include external library functions in exposed contract

--- src/core.ts
+++ src/core.ts
@@ -1,11 +1,17 @@
-import hre from 'hardhat';
-import path  from 'path';
-import 'array.prototype.flatmap/auto';
+import hre from "hardhat";
+import path from "path";
+import "array.prototype.flatmap/auto";
 
-import { SourceUnit, ContractDefinition, FunctionDefinition, VariableDeclaration, StorageLocation } from 'solidity-ast';
-import { findAll } from 'solidity-ast/utils';
-import { formatLines, spaceBetween } from './utils/format-lines';
-import { FileContent, ResolvedFile } from 'hardhat/types';
+import {
+  SourceUnit,
+  ContractDefinition,
+  FunctionDefinition,
+  VariableDeclaration,
+  StorageLocation,
+} from "solidity-ast";
+import { findAll } from "solidity-ast/utils";
+import { formatLines, spaceBetween } from "./utils/format-lines";
+import { FileContent, ResolvedFile } from "hardhat/types";
 
 export interface SolcOutput {
   sources: {
@@ -19,10 +25,13 @@ export interface SolcOutput {
 const rootPath = hre.config.paths.root;
 const sourcesPath = hre.config.paths.sources;
 const rootRelativeSourcesPath = path.relative(rootPath, sourcesPath);
-export const exposedPath = path.join(rootPath, 'contracts-exposed');
-const exposedVersionPragma = '>=0.6.0';
+export const exposedPath = path.join(rootPath, "contracts-exposed");
+const exposedVersionPragma = ">=0.6.0";
 
-export function getExposed(solcOutput: SolcOutput, isUserFile: (sourceName: string) => boolean): Map<string, ResolvedFile> {
+export function getExposed(
+  solcOutput: SolcOutput,
+  isUserFile: (sourceName: string) => boolean
+): Map<string, ResolvedFile> {
   const res = new Map<string, ResolvedFile>();
   const contractMap = mapContracts(solcOutput);
 
@@ -30,24 +39,35 @@ export function getExposed(solcOutput: SolcOutput, isUserFile: (sourceName: stri
     if (!isUserFile(ast.absolutePath)) {
       continue;
     }
-    const destPath = path.join(exposedPath, path.relative(rootRelativeSourcesPath, ast.absolutePath));
+    const destPath = path.join(
+      exposedPath,
+      path.relative(rootRelativeSourcesPath, ast.absolutePath)
+    );
     res.set(destPath, getExposedFile(destPath, ast, contractMap));
   }
 
   return res;
 }
 
-function getExposedFile(absolutePath: string, ast: SourceUnit, contractMap: ContractMap): ResolvedFile {
+function getExposedFile(
+  absolutePath: string,
+  ast: SourceUnit,
+  contractMap: ContractMap
+): ResolvedFile {
   const sourceName = path.relative(rootPath, absolutePath);
 
-  const inputPath = path.relative(path.dirname(absolutePath), ast.absolutePath).replace(/\\/g, '/');
+  const inputPath = path
+    .relative(path.dirname(absolutePath), ast.absolutePath)
+    .replace(/\\/g, "/");
   const content: FileContent = {
     rawContent: getExposedContent(ast, inputPath, contractMap),
     imports: [inputPath],
     versionPragmas: [exposedVersionPragma],
   };
 
-  const contentHash = createNonCryptographicHashBasedIdentifier(Buffer.from(content.rawContent)).toString('hex');
+  const contentHash = createNonCryptographicHashBasedIdentifier(
+    Buffer.from(content.rawContent)
+  ).toString("hex");
 
   return {
     absolutePath,
@@ -59,64 +79,89 @@ function getExposedFile(absolutePath: string, ast: SourceUnit, contractMap: Cont
   };
 }
 
-function getExposedContent(ast: SourceUnit, inputPath: string, contractMap: ContractMap): string {
+function getExposedContent(
+  ast: SourceUnit,
+  inputPath: string,
+  contractMap: ContractMap
+): string {
   return formatLines(
     ...spaceBetween(
-      ['// SPDX-License-Identifier: UNLICENSED'],
+      ["// SPDX-License-Identifier: UNLICENSED"],
       [`pragma solidity ${exposedVersionPragma};`],
       [`import "${inputPath}";`],
 
-      ...Array.from(findAll('ContractDefinition', ast), c => {
-        const isLibrary = c.contractKind === 'library';
+      ...Array.from(findAll("ContractDefinition", ast), (c) => {
+        const isLibrary = c.contractKind === "library";
         const contractHeader = [`contract X${c.name}`];
         if (!areFunctionsFullyImplemented(c, contractMap)) {
-          contractHeader.unshift('abstract');
+          contractHeader.unshift("abstract");
         }
         if (!isLibrary) {
           contractHeader.push(`is ${c.name}`);
         }
-        contractHeader.push('{');
+        contractHeader.push("{");
         return [
-          contractHeader.join(' '),
+          contractHeader.join(" "),
           spaceBetween(
             makeConstructor(c, contractMap),
-            ...getInternalFunctions(c, contractMap).filter(isExternalizable).map(fn => {
-              const args = getFunctionArguments(fn);
-              const header = [
-                'function',
-                `x${fn.name}(${args.map(a => `${a.type} ${a.name}`)})`,
-                'external',
-              ];
-              if (fn.stateMutability !== 'nonpayable') {
-                header.push(fn.stateMutability);
-              }
-              if (fn.returnParameters.parameters.length > 0) {
-                header.push(`returns (${fn.returnParameters.parameters.map(p => getType(p, 'memory')).join(', ')})`);
-              }
-              header.push('{');
-              return [
-                header.join(' '), [
-                  `return ${isLibrary ? c.name : 'super'}.${fn.name}(${args.map(a => a.name)});`
-                ], `}`,
-              ];
-            }),
+            ...getFunctions(c, contractMap, isLibrary)
+              .filter(isExternalizable)
+              .map((fn) => {
+                const args = getFunctionArguments(fn);
+                const header = [
+                  "function",
+                  `x${fn.name}(${args.map((a) => `${a.type} ${a.name}`)})`,
+                  "external",
+                ];
+                if (fn.stateMutability !== "nonpayable") {
+                  header.push(fn.stateMutability);
+                }
+                if (fn.returnParameters.parameters.length > 0) {
+                  header.push(
+                    `returns (${fn.returnParameters.parameters
+                      .map((p) => getType(p, "memory"))
+                      .join(", ")})`
+                  );
+                }
+                header.push("{");
+                return [
+                  header.join(" "),
+                  [
+                    `return ${isLibrary ? c.name : "super"}.${
+                      fn.name
+                    }(${args.map((a) => a.name)});`,
+                  ],
+                  `}`,
+                ];
+              })
           ),
           `}`,
-        ]
-      }),
+        ];
+      })
     )
-  )
+  );
 }
 
 // Note this is not the same as contract.fullyImplemented, because this does
 // not consider missing constructor calls. We don't use contract.abstract
 // because even if a user declares a contract abstract, we want to make it
 // concrete if it is possible.
-function areFunctionsFullyImplemented(contract: ContractDefinition, contractMap: ContractMap): boolean {
-  const parents = contract.linearizedBaseContracts.map(id => mustGet(contractMap, id));
-  const abstractFunctionIds = new Set(parents.flatMap(p => [...findAll('FunctionDefinition', p)].filter(f => !f.implemented).map(f => f.id)));
+function areFunctionsFullyImplemented(
+  contract: ContractDefinition,
+  contractMap: ContractMap
+): boolean {
+  const parents = contract.linearizedBaseContracts.map((id) =>
+    mustGet(contractMap, id)
+  );
+  const abstractFunctionIds = new Set(
+    parents.flatMap((p) =>
+      [...findAll("FunctionDefinition", p)]
+        .filter((f) => !f.implemented)
+        .map((f) => f.id)
+    )
+  );
   for (const p of parents) {
-    for (const f of findAll(['FunctionDefinition', 'VariableDeclaration'], p)) {
+    for (const f of findAll(["FunctionDefinition", "VariableDeclaration"], p)) {
       for (const b of f.baseFunctions ?? []) {
         abstractFunctionIds.delete(b);
       }
@@ -125,14 +170,27 @@ function areFunctionsFullyImplemented(contract: ContractDefinition, contractMap:
   return abstractFunctionIds.size === 0;
 }
 
-function makeConstructor(contract: ContractDefinition, contractMap: ContractMap): string[] {
-  const parents = contract.linearizedBaseContracts.map(id => mustGet(contractMap, id)).reverse();
-  const parentsWithConstructor = parents.filter(c => getConstructor(c)?.parameters.parameters.length);
-  const initializedParentIds = new Set(parents.flatMap(p => [
-    ...p.baseContracts.filter(c => c.arguments?.length).map(c => c.id),
-    ...getConstructor(p)?.modifiers.map(m => m.modifierName.referencedDeclaration).filter(notNull) ?? [],
-  ]));
-  const uninitializedParents = parentsWithConstructor.filter(c => !initializedParentIds.has(c.id));
+function makeConstructor(
+  contract: ContractDefinition,
+  contractMap: ContractMap
+): string[] {
+  const parents = contract.linearizedBaseContracts
+    .map((id) => mustGet(contractMap, id))
+    .reverse();
+  const parentsWithConstructor = parents.filter(
+    (c) => getConstructor(c)?.parameters.parameters.length
+  );
+  const initializedParentIds = new Set(
+    parents.flatMap((p) => [
+      ...p.baseContracts.filter((c) => c.arguments?.length).map((c) => c.id),
+      ...(getConstructor(p)
+        ?.modifiers.map((m) => m.modifierName.referencedDeclaration)
+        .filter(notNull) ?? []),
+    ])
+  );
+  const uninitializedParents = parentsWithConstructor.filter(
+    (c) => !initializedParentIds.has(c.id)
+  );
 
   const missingArguments = new Map<string, string>(); // name -> type
   const parentArguments = new Map<string, string[]>();
@@ -140,8 +198,10 @@ function makeConstructor(contract: ContractDefinition, contractMap: ContractMap)
   for (const c of uninitializedParents) {
     const args = [];
     for (const a of getConstructor(c)!.parameters.parameters) {
-      const name = missingArguments.has(a.name) ? `${c.name}_${a.name}` : a.name;
-      const type = getType(a, 'memory');
+      const name = missingArguments.has(a.name)
+        ? `${c.name}_${a.name}`
+        : a.name;
+      const type = getType(a, "memory");
       missingArguments.set(name, type);
       args.push(name);
     }
@@ -149,16 +209,22 @@ function makeConstructor(contract: ContractDefinition, contractMap: ContractMap)
   }
   return [
     [
-      `constructor(${[...missingArguments].map(([name, type]) => `${type} ${name}`).join(', ')})`,
-      ...uninitializedParents.map(p => `${p.name}(${mustGet(parentArguments, p.name).join(', ')})`),
-      '{}'
-    ].join(' '),
+      `constructor(${[...missingArguments]
+        .map(([name, type]) => `${type} ${name}`)
+        .join(", ")})`,
+      ...uninitializedParents.map(
+        (p) => `${p.name}(${mustGet(parentArguments, p.name).join(", ")})`
+      ),
+      "{}",
+    ].join(" "),
   ];
 }
 
-function getConstructor(contract: ContractDefinition): FunctionDefinition | undefined {
-  for (const fnDef of findAll('FunctionDefinition', contract)) {
-    if (fnDef.kind === 'constructor') {
+function getConstructor(
+  contract: ContractDefinition
+): FunctionDefinition | undefined {
+  for (const fnDef of findAll("FunctionDefinition", contract)) {
+    if (fnDef.kind === "constructor") {
       return fnDef;
     }
   }
@@ -169,7 +235,11 @@ function notNull<T>(value: T): value is NonNullable<T> {
 }
 
 function isExternalizable(fnDef: FunctionDefinition): boolean {
-  return fnDef.kind !== 'constructor' && fnDef.implemented && fnDef.parameters.parameters.every(p => p.storageLocation !== 'storage');
+  return (
+    fnDef.kind !== "constructor" &&
+    fnDef.implemented &&
+    fnDef.parameters.parameters.every((p) => p.storageLocation !== "storage")
+  );
 }
 
 interface Argument {
@@ -179,18 +249,23 @@ interface Argument {
 
 function getFunctionArguments(fnDef: FunctionDefinition): Argument[] {
   return fnDef.parameters.parameters.map((p, i) => {
-    const type = getType(p, 'calldata');
+    const type = getType(p, "calldata");
     const name = p.name || `arg${i}`;
     return { type, name };
   });
 }
 
-function getType(varDecl: VariableDeclaration, location: StorageLocation = varDecl.storageLocation): string {
+function getType(
+  varDecl: VariableDeclaration,
+  location: StorageLocation = varDecl.storageLocation
+): string {
   const { typeString, typeIdentifier } = varDecl.typeDescriptions;
-  if (typeof typeString !== 'string' || typeof typeIdentifier !== 'string') {
-    throw new Error('Missing type information');
+  if (typeof typeString !== "string" || typeof typeIdentifier !== "string") {
+    throw new Error("Missing type information");
   }
-  const type = typeString.replace(/^(struct|enum|contract) /, '') + (typeIdentifier.endsWith('_ptr') ? ` ${location}` : '');
+  const type =
+    typeString.replace(/^(struct|enum|contract) /, "") +
+    (typeIdentifier.endsWith("_ptr") ? ` ${location}` : "");
   return type;
 }
 
@@ -200,7 +275,7 @@ function mapContracts(solcOutput: SolcOutput): ContractMap {
   const res: ContractMap = new Map();
 
   for (const { ast } of Object.values(solcOutput.sources)) {
-    for (const contract of findAll('ContractDefinition', ast)) {
+    for (const contract of findAll("ContractDefinition", ast)) {
       res.set(contract.id, contract);
     }
   }
@@ -208,15 +283,23 @@ function mapContracts(solcOutput: SolcOutput): ContractMap {
   return res;
 }
 
-function getInternalFunctions(contract: ContractDefinition, contractMap: ContractMap): FunctionDefinition[] {
-  const parents = contract.linearizedBaseContracts.map(id => mustGet(contractMap, id));
+function getFunctions(
+  contract: ContractDefinition,
+  contractMap: ContractMap,
+  isLibrary: boolean
+): FunctionDefinition[] {
+  const parents = contract.linearizedBaseContracts.map((id) =>
+    mustGet(contractMap, id)
+  );
 
   const overriden = new Set<number>();
   const res = [];
 
   for (const parent of parents) {
-    for (const fn of findAll('FunctionDefinition', parent)) {
-      if (fn.visibility === 'internal' && !overriden.has(fn.id)) {
+    for (const fn of findAll("FunctionDefinition", parent)) {
+      if (fn.visibility === "internal" && !overriden.has(fn.id)) {
+        res.push(fn);
+      } else if (isLibrary && !overriden.has(fn.id)) {
         res.push(fn);
       }
       for (const b of fn.baseFunctions ?? []) {
@@ -231,12 +314,12 @@ function getInternalFunctions(contract: ContractDefinition, contractMap: Contrac
 function mustGet<K, V>(map: Map<K, V>, key: K): V {
   const value = map.get(key);
   if (value === undefined) {
-    throw new Error('Key not found');
+    throw new Error("Key not found");
   }
   return value;
 }
 
 function createNonCryptographicHashBasedIdentifier(input: Buffer): Buffer {
-  const { createHash } = require("crypto") as typeof import('crypto');
+  const { createHash } = require("crypto") as typeof import("crypto");
   return createHash("md5").update(input).digest();
 }
